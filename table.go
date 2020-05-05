1553cb760d6e6baed09a60f5d6d48db411e7693d
Michel Hass <4216942+michaelshass@users.noreply.github.com>
2020-05-05 14:38:35 +0900

    Remove comment

--- table.go
+++ table.go
@@ -1,15 +1,5 @@
 package main
 
-// +const uint32_t PAGE_SIZE = 4096;
-// +#define TABLE_MAX_PAGES 100
-// +const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-// +const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-// +
-// +typedef struct {
-// +  uint32_t num_rows;
-// +  void* pages[TABLE_MAX_PAGES];
-// +} Table;
-
 const (
 	pageSize      = 1000 * 4096
 	rowsPerPage   = pageSize / rowSize
